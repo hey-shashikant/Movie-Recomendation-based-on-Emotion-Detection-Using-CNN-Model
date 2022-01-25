859d740f2dbccb4365c0d4df495bf15d03da8ff4
Shashikant Solanki <90645693+hey-shashikant@users.noreply.github.com>
2022-01-25 15:09:54 +0530

    Accessing a particular emotion based on it's frequency

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter.dialog import DIALOG_ICON
 from typing import Dict
 from keras.models import load_model
 from time import sleep
@@ -5,55 +6,77 @@ from keras.preprocessing.image import img_to_array
 from keras.preprocessing import image
 import cv2
 import numpy as np
+from collections import defaultdict
 
 face_classifier = cv2.CascadeClassifier(r'D:\Emotion_Detection_CNN-main-main\haarcascade_frontalface_default.xml')
 classifier =load_model(r'D:\Emotion_Detection_CNN-main-main\model.h5')
 
 emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
 
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 
-# Dict = {}
+Dict = defaultdict(lambda:0)
 
-while True:
-    # Dict = {}
-    _, frame = cap.read()
-    labels = []
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    faces = face_classifier.detectMultiScale(gray)
+def solve():
+    cap = cv2.VideoCapture(0)
+    while True:
+        # Dict = {}
+        _, frame = cap.read()
+        labels = []
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faces = face_classifier.detectMultiScale(gray)
 
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
-        roi_gray = gray[y:y+h,x:x+w]
-        roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+            roi_gray = gray[y:y+h,x:x+w]
+            roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
 
 
 
-        if np.sum([roi_gray])!=0:
-            roi = roi_gray.astype('float')/255.0
-            roi = img_to_array(roi)
-            roi = np.expand_dims(roi,axis=0)
+            if np.sum([roi_gray])!=0:
+                roi = roi_gray.astype('float')/255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi,axis=0)
 
-            prediction = classifier.predict(roi)[0]
-            label=emotion_labels[prediction.argmax()]
-            # Storing the frequency of the emotion detected.
-            # if label in Dict:
-            #     Dict[label] += 1
-            # else:
-            #     Dict[label] = 1
-            # print(label)
-            label_position = (x,y)
-            cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
-        else:
-            cv2.putText(frame,'No Faces',(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
-    cv2.imshow('Emotion Detector',frame)
-    # print(labels)
-    # Printing all the emotions detected and their frequency.
-    # print(Dict)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+                prediction = classifier.predict(roi)[0]
+                label=emotion_labels[prediction.argmax()]
+                # Storing the frequency of the emotion detected.
+                Dict[label]+=1
+                # print(label)
+                label_position = (x,y)
+                cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+            else:
+                cv2.putText(frame,'No Facqes',(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+        cv2.imshow('Emotion Detector',frame)
+        # print(labels)
+        # Printing all the emotions detected and their frequency.
+        # print(Dict)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
 # print(Dict)
 
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    print ("Executed when invoked directly")
+    solve()
+
+    emotion_name = ""
+    maxi = -1000000
+    for i in Dict:
+        if Dict[i] > maxi and i != "Neutral":
+            emotion_name = i;
+            maxi = Dict[i]
+    print("Emotion Name : ",emotion_name) 
+
+# solve()
+
+
+# cap.release()
+# cv2.destroyAllWindows()
+# cap.release()
+print(Dict)
+# cap.release()
+# cv2.destroyAllWindows()
+# print(Dict)
