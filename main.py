1138f076d3196430e9fcd2b884ac149b3a72e253
Shashikant Solanki <90645693+hey-shashikant@users.noreply.github.com>
2022-01-25 15:26:49 +0530

    Merging the movie recommendation with emotion detection

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from keras.preprocessing import image
 import cv2
 import numpy as np
 from collections import defaultdict
+from bs4 import BeautifulSoup as SOUP
+import re
+import requests as HTTP
 
 face_classifier = cv2.CascadeClassifier(r'D:\Emotion_Detection_CNN-main-main\haarcascade_frontalface_default.xml')
 classifier =load_model(r'D:\Emotion_Detection_CNN-main-main\model.h5')
@@ -14,6 +17,12 @@ classifier =load_model(r'D:\Emotion_Detection_CNN-main-main\model.h5')
 emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
 
 # cap = cv2.VideoCapture(0)
+
+
+
+
+
+
 
 Dict = defaultdict(lambda:0)
 
@@ -53,10 +62,72 @@ def solve():
         # print(Dict)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    cap.release()
-    cv2.destroyAllWindows()
+    # cap.release()
+    # cv2.destroyAllWindows()
 
 # print(Dict)
+
+# Main Function for scraping
+def test(emotion):
+  
+    # IMDb Url for Drama genre of
+    # movie against emotion Sad
+    if(emotion == "Sad"):
+        urlhere = 'http://www.imdb.com/search/title?genres=drama&title_type=feature&sort=moviemeter, asc'
+  
+    # IMDb Url for Musical genre of
+    # movie against emotion Disgust
+    elif(emotion == "Disgust"):
+        urlhere = 'http://www.imdb.com/search/title?genres=musical&title_type=feature&sort=moviemeter, asc'
+  
+    # IMDb Url for Family genre of
+    # movie against emotion Anger
+    elif(emotion == "Anger"):
+        urlhere = 'http://www.imdb.com/search/title?genres=family&title_type=feature&sort=moviemeter, asc'
+  
+    # IMDb Url for Thriller genre of
+    # movie against emotion Anticipation
+    elif(emotion == "Anticipation"):
+        urlhere = 'http://www.imdb.com/search/title?genres=thriller&title_type=feature&sort=moviemeter, asc'
+  
+    # IMDb Url for Sport genre of
+    # movie against emotion Fear
+    elif(emotion == "Fear"):
+        urlhere = 'http://www.imdb.com/search/title?genres=sport&title_type=feature&sort=moviemeter, asc'
+  
+    # IMDb Url for Thriller genre of
+    # movie against emotion Enjoyment
+    elif(emotion == "Enjoyment"):
+        urlhere = 'http://www.imdb.com/search/title?genres=thriller&title_type=feature&sort=moviemeter, asc'
+  
+    # IMDb Url for Western genre of
+    # movie against emotion Trust
+    elif(emotion == "Trust"):
+        urlhere = 'http://www.imdb.com/search/title?genres=western&title_type=feature&sort=moviemeter, asc'
+  
+    # IMDb Url for Film_noir genre of
+    # movie against emotion Surprise
+    elif(emotion == "Surprise"):
+        urlhere = 'http://www.imdb.com/search/title?genres=film_noir&title_type=feature&sort=moviemeter, asc'
+  
+    # HTTP request to get the data of
+    # the whole page
+    response = HTTP.get(urlhere)
+    data = response.text
+  
+    # Parsing the data using
+    # BeautifulSoup
+    soup = SOUP(data, "lxml")
+  
+    # Extract movie titles from the
+    # data using regex
+    title = soup.find_all("a", attrs = {"href" : re.compile(r'\/title\/tt+\d*\/')})
+    return title
+  
+
+
+
+
 
 if __name__ == "__main__":
     print ("Executed when invoked directly")
@@ -70,13 +141,44 @@ if __name__ == "__main__":
             maxi = Dict[i]
     print("Emotion Name : ",emotion_name) 
 
+
+    a = test(emotion_name)
+    count = 0
+  
+    if(emotion == "Disgust" or emotion == "Anger" or emotion=="Surprise"):
+        for i in a:
+  
+            # Splitting each line of the
+            # IMDb data to scrape movies
+            tmp = str(i).split('>;')
+  
+            if(len(tmp) == 3):
+                print(tmp[1][:-3])
+  
+            if(count > 13):
+                break
+            count += 1
+    else:
+        for i in a:
+            tmp = str(i).split('>')
+  
+            if(len(tmp) == 3):
+                print(tmp[1][:-3])
+  
+            if(count > 11):
+                break
+            count+=1
+
 # solve()
 
 
 # cap.release()
 # cv2.destroyAllWindows()
 # cap.release()
-print(Dict)
+# print(Dict)
 # cap.release()
 # cv2.destroyAllWindows()
 # print(Dict)
+
+cap.release()
+cv2.destroyAllWindows()
